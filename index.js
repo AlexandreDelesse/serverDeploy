4ebc63daa2cff6bdd4aa14414fc912a76ea821a6
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2021-12-04 14:03:13 +0100

    test on exec cmd

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const { exec } = require('child_process')
 const app = express()
 const port = 3000
 
+const cleanDocker = () => {
+  exec('docker kill $(docker ps -q)', (error, stdout, stderr) => {
+    if (error) res.send(`error : ${error.message}`)
+    if (stderr) res.send(`stderr : ${stderr}`)
+    res.send(`stdout ${stdout}`)
+  })
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  cleanDocker()
 })
 
 app.listen(port, () => {
