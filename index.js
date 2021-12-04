1f957851d8163a1f55103e11881cfb27105bc0c6
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2021-12-04 14:11:38 +0100

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 const cleanDocker = (req, res) => {
-  exec('docker kill $(docker ps -q)', (error, stdout, stderr) => {
+  exec('docker ps -q', (error, stdout, stderr) => {
     if (error) res.send(`error : ${error.message}`)
     if (stderr) res.send(`stderr : ${stderr}`)
     res.send(`stdout ${stdout}`)
@@ -12,14 +12,10 @@ const cleanDocker = (req, res) => {
 }
 
 app.get('/', (req, res) => {
-  try {
-    exec('docker kill $(docker ps -q)', (error, stdout, stderr) => {
-      if (error) res.send(`error : ${error.message}`)
-      if (stderr) res.send(`stderr : ${stderr}`)
-      res.send(`stdout ${stdout}`)
-    })
-  } catch {
-    console.log('poin poin poin...')
+  ;(error, stdout, stderr) => {
+    if (error) res.send(`error : ${error.message}`)
+    if (stderr) res.send(`stderr : ${stderr}`)
+    res.send(`stdout ${stdout}`)
   }
 })
 
