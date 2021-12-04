51ba5a0bbe4ef7dfa0dd72b2fa14b85315b626d3
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2021-12-04 14:45:43 +0100

    test

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ const { exec } = require('child_process')
 const app = express()
 const port = 3000
 
-const cleanDocker = (req, res) => {
+const killContainer = (req, res, next) => {
   exec('docker ps -q', (error, stdout, stderr) => {
-    if (error) res.send(`error : ${error.message}`)
-    if (stderr) res.send(`stderr : ${stderr}`)
-    res.send(`stdout ${stdout}`)
+    if (error) console.log(`error : ${error.message}`)
+    if (stderr) console.log(`stderr : ${stderr}`)
+    if (stdout) {
+      exec('docker kill $(docker ps -q)', (error, stdout, stderr) => {
+        if (error) console.log(`error : ${error.message}`)
+        if (stderr) console.log(`stderr : ${stderr}`)
+        if (stdout) next()
+      })
+    } else {
+      next()
+    }
   })
 }
 
 app.get('/', (req, res) => {
-  exec('sh deploy.sh', (error, stdout, stderr) => {
-    if (error) console.log(`error : ${error.message}`)
-    if (stderr) console.log(`stderr : ${stderr}`)
-    res.send(`stdout ${stdout}`)
+  killContainer(req, res, () => {
+    console.log('docker kill ok !')
   })
 })
 
