26040457d71699b483e3b93ab2b46f9c815a7b86
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2021-12-04 14:04:42 +0100

    fix bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process')
 const app = express()
 const port = 3000
 
-const cleanDocker = () => {
+const cleanDocker = (req, res) => {
   exec('docker kill $(docker ps -q)', (error, stdout, stderr) => {
     if (error) res.send(`error : ${error.message}`)
     if (stderr) res.send(`stderr : ${stderr}`)
@@ -12,7 +12,7 @@ const cleanDocker = () => {
 }
 
 app.get('/', (req, res) => {
-  cleanDocker()
+  cleanDocker(req, res)
 })
 
 app.listen(port, () => {
